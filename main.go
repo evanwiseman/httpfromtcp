94bb18dbdb5a9bd2d7b1081a6a573c3bf285fc55
evanwiseman <evanwiseman2000@gmail.com>
2025-10-18 22:09:42 -0700

    add read file in 8 byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	filename = "messages.txt"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(filename)
+	if err != nil {
+		os.Exit(1)
+	}
+	for {
+		// Byte slice of size 8
+		buffer := make([]byte, 8)
+
+		// Read file into buffer
+		_, err := file.Read(buffer)
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			os.Exit(2)
+		}
+
+		fmt.Printf("read: %s\n", string(buffer))
+	}
 }
