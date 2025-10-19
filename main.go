20dc3619cd2e47b7790479728fda89cf4ea6b81a
evanwiseman <evanwiseman2000@gmail.com>
2025-10-18 22:27:40 -0700

    update read 8 byte chunks -> entire lines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 const (
@@ -15,12 +16,15 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+
+	// Store the current line to output
+	var currentLine string
 	for {
 		// Byte slice of size 8
 		buffer := make([]byte, 8)
 
 		// Read file into buffer
-		_, err := file.Read(buffer)
+		n, err := file.Read(buffer)
 
 		if err == io.EOF {
 			break
@@ -29,6 +33,24 @@ func main() {
 			os.Exit(2)
 		}
 
-		fmt.Printf("read: %s\n", string(buffer))
+		// Split buffer into parts and add first part to current line
+		parts := strings.Split(string(buffer[:n]), "\n")
+
+		// Append
+		currentLine += parts[0]
+
+		// iterate through parts
+		for i, part := range parts {
+			if i == 0 {
+				continue
+			}
+			fmt.Printf("read: %s\n", currentLine) // print current line
+			currentLine = ""                      // reset current line
+			currentLine += part                   // add to current line
+		}
+	}
+
+	if currentLine != "" { // ensure we've read everything
+		fmt.Printf("read: %s\n", currentLine)
 	}
 }
